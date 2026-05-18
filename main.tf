0a1b8c6ca27aa337db11ac8e8ab071ac001a0f7b
VathanakSol <vathanaksol1605@gmail.com>
2026-05-18 11:41:13 +0700

    fix main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,15 @@ resource "aws_s3_bucket_website_configuration" "website" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "static_site" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false  
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "website" {
   bucket = aws_s3_bucket.website.id
 
