bf67d08ad1b9c4bf742e83003ce5abab01ca29c0
VathanakSol <vathanaksol1605@gmail.com>
2026-05-18 13:30:02 +0700

    Update repository

--- main.tf
+++ main.tf
@@ -35,6 +35,8 @@ resource "aws_s3_bucket_public_access_block" "website" {
 resource "aws_s3_bucket_policy" "website" {
   bucket = aws_s3_bucket.website.id
 
+  depends_on = [aws_s3_bucket_public_access_block.website]
+
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
